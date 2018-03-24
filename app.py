5e01e9cc8cd44091db804d4be789462ff41b4d9a
ALEX-LAPTOP\Alex <7989990@gmail.com>
2018-03-24 12:43:39 -0700

    initial_commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
+
+
+app = Flask(__name__)
+
+app.secret_key = "secret_key"
+
+
+@app.route("/")
+def home():
+    # return "Home"
+    return render_template("index.html")
+
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            error = "Invalid credentials"
+        else:
+            session["logged_in"] = True
+            flash("You were just logged in!")
+            return redirect(url_for("home"))
+    return render_template("login-form.html", error=error)
+
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash("You were just logged out!")
+    return redirect(url_for("welcome"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
